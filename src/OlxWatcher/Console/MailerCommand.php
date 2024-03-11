a6e721c1a6a9139f8a9c88c00c6ad612cbf269a3
Igor-ad <admin@autodoctor.od.ua>
2024-03-11 10:40:04 +0200

    Refactoring MailerCommand & WatcherCommand

--- src/OlxWatcher/Console/MailerCommand.php
+++ src/OlxWatcher/Console/MailerCommand.php
@@ -9,8 +9,10 @@ use Autodoctor\OlxWatcher\Mail\Mailer;
 require __DIR__ . '/../../../vendor/autoload.php';
 
 try {
+    echo 'Mailer started at ' . date('Y-m-d H:i:s') . PHP_EOL;
     $mailer = new Mailer();
     $mailer();
+    echo 'Mailer stopped at ' . date('Y-m-d H:i:s') . PHP_EOL;
 } catch (\Exception $e) {
     echo 'Mail error: ',  $e->getMessage(), "\n";
 }
--- src/OlxWatcher/Console/WatcherCommand.php
+++ src/OlxWatcher/Console/WatcherCommand.php
@@ -9,8 +9,10 @@ use Autodoctor\OlxWatcher\WatcherService;
 require __DIR__ . '/../../../vendor/autoload.php';
 
 try {
+    echo 'Watcher started at ' . date('Y-m-d H:i:s') . PHP_EOL;
     $watcher = new WatcherService();
     $watcher();
+    echo 'Watcher stopped at ' . date('Y-m-d H:i:s') . PHP_EOL;
 } catch (\Exception $e) {
     echo 'Watcher error: ',  $e->getMessage(), "\n";
 }
