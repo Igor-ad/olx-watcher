2f9f62bdd6ccf9b4befeeab30f032260d34c1b62
Igor-ad <admin@autodoctor.od.ua>
2024-03-16 10:51:38 +0200

    Refactoring FileProcessor & CacheFileService

--- src/OlxWatcher/CacheFileService.php
+++ src/OlxWatcher/CacheFileService.php
@@ -13,7 +13,7 @@ class CacheFileService implements CacheInterface
      */
     public static function get(string $key): mixed
     {
-        return json_decode(FileProcessor::readOnly($key), true);
+        return json_decode(FileProcessor::getContent($key), true);
     }
 
     /**
@@ -21,6 +21,6 @@ class CacheFileService implements CacheInterface
      */
     public static function set(string $key, mixed $data): void
     {
-        FileProcessor::writeNewFile($key, json_encode($data));
+        FileProcessor::putContent($key, json_encode($data));
     }
 }
--- src/OlxWatcher/FileProcessor.php
+++ src/OlxWatcher/FileProcessor.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Autodoctor\OlxWatcher;
 
 use Autodoctor\OlxWatcher\Exceptions\WatcherException;
-use Autodoctor\OlxWatcher\Enums\AccessLevelEnum;
 
 class FileProcessor
 {
@@ -16,82 +15,22 @@ class FileProcessor
     {
         $data = file_get_contents($fileName);
 
-        if ($data) {
-            return $data;
+        if ($data === false) {
+            throw new WatcherException(sprintf('Error reading %s file.', $fileName));
         }
-        throw new WatcherException(sprintf('There is no data in the %s file.', $fileName));
+        return $data;
     }
 
     /**
      * @throws WatcherException
      */
-    protected static function read(string $fileName, string $level): string
+    public static function putContent(string $fileName, mixed $data): int
     {
-        $fp = fopen($fileName, $level);
-        if (flock($fp, LOCK_SH | LOCK_NB)) {
-            $data = self::getContent($fileName);
-            flock($fp, LOCK_UN);
-            fclose($fp);
-            return $data;
+        $result = file_put_contents($fileName, $data);
+
+        if ($result === false) {
+            throw new WatcherException(sprintf('Error writing to file %s.', $fileName));
         }
-            throw new WatcherException(sprintf('Failed to obtain lock on file %s', $fileName));
-    }
-
-    /**
-     * @throws WatcherException
-     */
-    public static function reader(string $fileName, string $accessLevel): string
-    {
-        if (file_exists($fileName)) {
-            return self::read($fileName, $accessLevel);
-        }
-        throw new WatcherException(sprintf('%s file not found', $fileName));
-    }
-
-    /**
-     * @throws WatcherException
-     */
-    public static function readOnly(string $fileName): string
-    {
-        return self::reader($fileName, AccessLevelEnum::READ_ONLY);
-    }
-
-    public static function simpleDelete(string $fileName): void
-    {
-        unlink($fileName);
-    }
-
-    /**
-     * @throws WatcherException
-     */
-    protected static function write(string $fileName, string $accessLevel, string $data): int
-    {
-        $fp = fopen($fileName, $accessLevel);
-        if (flock($fp, LOCK_EX | LOCK_NB)) {
-            $data = fwrite($fp, $data);
-            if (!$data) {
-                throw new WatcherException(sprintf('The file %s is not writable.', $fileName));
-            }
-            flock($fp, LOCK_UN);
-            fclose($fp);
-            return $data;
-        }
-        throw new WatcherException(sprintf('The %s file is not blocked.', $fileName));
-    }
-
-    /**
-     * @throws WatcherException
-     */
-    public static function writeNewFile(string $fileName, string $data): int
-    {
-        return self::write($fileName, AccessLevelEnum::NEW_WRITE, $data);
-    }
-
-    /**
-     * @throws WatcherException
-     */
-    public static function writeToTheEnd(string $fileName, string $data): int
-    {
-        return self::write($fileName, AccessLevelEnum::WRITE_END, $data);
+        return $result;
     }
 }
