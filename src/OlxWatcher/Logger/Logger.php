d371235c1b8aa03069232d6308ea31f1a7c617f4
Igor-ad <admin@autodoctor.od.ua>
2024-03-29 12:09:27 +0200

    Refactoring Logger

--- src/OlxWatcher/Logger/Logger.php
+++ src/OlxWatcher/Logger/Logger.php
@@ -8,6 +8,8 @@ use Autodoctor\OlxWatcher\Enums\FilesEnum;
 use Autodoctor\OlxWatcher\Exceptions\WatcherException;
 use Autodoctor\OlxWatcher\FileProcessor;
 use Psr\Log\AbstractLogger;
+use Psr\Log\InvalidArgumentException;
+use Psr\Log\LogLevel;
 
 class Logger extends AbstractLogger
 {
@@ -20,11 +22,33 @@ class Logger extends AbstractLogger
     }
 
     /**
-     * @throws WatcherException
+     * @throws WatcherException|InvalidArgumentException
      */
     public function log($level, string|\Stringable $message, array $context = []): void
     {
+        if (!$this->checkValidArgument($level)) {
+            throw new InvalidArgumentException('This logging level is not available.');
+        }
         FileProcessor::putContent(
             $this->logFile, $this->toString($level, $message, $context), FILE_APPEND);
+    }
+
+    private function checkValidArgument(string $level): bool
+    {
+        return in_array($level, $this->levelToArray(), true);
+    }
+
+    private function levelToArray(): array
+    {
+        return [
+            LogLevel::ALERT,
+            LogLevel::CRITICAL,
+            LogLevel::DEBUG,
+            LogLevel::EMERGENCY,
+            LogLevel::ERROR,
+            LogLevel::INFO,
+            LogLevel::NOTICE,
+            LogLevel::WARNING,
+        ];
     }
 }
